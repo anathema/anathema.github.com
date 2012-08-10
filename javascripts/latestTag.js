ff5eb67fcc9f5262c758404cdec8a88e3a17d3e5
Urs Reupke <ursreupke@gmail.com>
2012-08-10 21:10:10 +0200

    Shows target platform

--- javascripts/latestTag.js
+++ javascripts/latestTag.js
@@ -1,11 +1,16 @@
 $( document ).ready( function () {
-   var  downloadType = "Zip";
+   var downloadType = "Zip";
    var fileExtension = "zip";
+   var targetPlatform = "all systems"
    if (navigator.appVersion.indexOf("Win")!=-1) {
      downloadType="Win";
      fileExtension="exe";
+	 targetPlatform = "Windows"
    }
-   if (navigator.appVersion.indexOf("Mac")!=-1) downloadType="Mac";
+	if (navigator.appVersion.indexOf("Mac")!=-1){
+		downloadType="Mac";
+		targetPlatform="Mac OS";
+	}
    $.ajax( {
     url : "https://api.github.com/repos/anathema/anathema/tags",
     dataType : "jsonp",
@@ -16,10 +21,10 @@ $( document ).ready( function () {
 			var tag = tags[tagKey];
 			var taggedVersion = tag.name.substring(1);
             if (taggedVersion > latestVersion) {
-               latestVersion = taggedVersion;			   
+               latestVersion = taggedVersion;
             }
          }
-      $( '.latestVersion' ).append(latestVersion);
+      $( '.latestVersion' ).append(latestVersion+" for "+targetPlatform);
 	  $( '.linkToChangelog' ).attr("href", 'https://github.com/anathema/anathema/blob/v'+latestVersion+'/Development_Documentation/Distribution/English/versions.md');
 	  $( '.linkToLatestVersion').attr("href", 'http://anathema.butatopanto.de:8081/full/'+latestVersion+'/Anathema_'+downloadType+'_v'+latestVersion+'.'+fileExtension);
     }
